121d44df9e50339e2e9e681adfe909a8dfd039c1
min1025 <shadow20001025@gmail.com>
2023-11-23 23:17:26 +0800

    update 完成

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ echo "<table border='1'>
   <tr>
     <th>PersonID</th>
     <th>FirstName</th>
+    <th>LastName</th>
     <th>Age</th>
     <th>UPDATE</th>
     <th>DELETE</th>
@@ -16,6 +17,7 @@ while ($row = $result->fetch_array()) {
   echo "<tr align='center'>
       <td>{$row['personID']}</td>
       <td>{$row['FirstName']}</td>
+      <td>{$row['LastName']}</td>
       <td>{$row['Age']}</td>
       <th><a href=\"update.php?personID={$row['personID']}&FirstName={$row['FirstName']}&LastName={$row['LastName']}&Age={$row['Age']}\">UPDATE</a></th>
       <th><a href=\"delete.php?personID={$row['personID']}\">DELETE</a></th>
