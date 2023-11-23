719e04bfc98de588712eb9e96c1916b66ba2c3c0
min1025 <shadow20001025@gmail.com>
2023-11-24 01:29:12 +0800

    隱藏personid欄位

--- update.php
+++ update.php
@@ -1,23 +1,4 @@
 <?php
-echo "<form method=\"get\">
-<table border='1'>
-<tr>
-<th>personID</th>
-<th>FirstName</th>
-<th>LastName</th>
-<th>Age</th>
-</tr>";
-echo "<tr>
-<td><input type='text' name='personID' value=$_GET[personID] readonly></td>
-<td><input type='text' name='FirstName' value=$_GET[FirstName]></td>
-<td><input type='text' name='LastName' value=$_GET[LastName]></td>
-<td><input type='text' name='Age' value=$_GET[Age]></td>
-</tr>";
-echo "<tr height=20>
-<td colspan='4' align=center><input type='submit' value='修改' name='submit'></td>
-</tr>";
-echo "</table></form>";
-
 include('sql_connect.php');
 $con->select_db('my_db');
 $result = $con->query('SELECT * FROM persons');
@@ -27,3 +8,21 @@ if (isset($_GET['submit'])) {
   $con->query($sql);
   header("location:index.php");
 }
+echo "<form method=\"get\">
+<table border='1'>
+<tr>
+<th hidden>personID</th>
+<th>FirstName</th>
+<th>LastName</th>
+<th>Age</th>
+</tr>";
+echo "<tr>
+<td hidden><input type='text' name='personID' value=$_GET[personID] readonly ></td>
+<td><input type='text' name='FirstName' value=$_GET[FirstName]></td>
+<td><input type='text' name='LastName' value=$_GET[LastName]></td>
+<td><input type='text' name='Age' value=$_GET[Age]></td>
+</tr>";
+echo "<tr>
+<td colspan='3' align=center><input type='submit' value='修改' name='submit'></td>
+</tr>";
+echo "</table></form>";
